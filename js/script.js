79923caa9bf863c3d44112b2dabb69b0a88e2fdc
Unknown <mathias.caelenberghe@student.howest.be>
2018-04-10 15:50:04 +0200

    slight changes

--- js/script.js
+++ js/script.js
@@ -23,11 +23,16 @@ function preload() {
     this.load.spritesheet('castle','assets/Kasteel.png', {frameWidth: 64, frameHeight: 64});
     this.load.spritesheet('peasant','assets/Peasant.png', {frameWidth: 64, frameHeight: 64});
     this.load.spritesheet('squirrel','assets/squirrel.png', {frameWidth: 64, frameHeight: 64});
+    this.load.spritesheet('knight','assets/Ridder.png', {frameWidth:64, frameHeight: 64});
 }
 
-function create() {
+function create() {;
     this.add.image(350,350,'ground');
     let center = this.add.sprite(350,350,'castle');
+    let knight = this.add.sprite(400,350,'knight');
+    knight.scaleX = 1.5;
+    knight.scaleY = 1.5
+
     let peasent = this.add.sprite(200,200,'peasant');
     let squirrel = this.add.sprite(100,200, 'squirrel');
 }
