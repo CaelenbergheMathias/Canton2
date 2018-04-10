afe90844438136d7d8be125b6dfb0df4aeb8de78
Unknown <mathias.caelenberghe@student.howest.be>
2018-04-10 15:12:37 +0200

    back to one file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 700,
+    height: 700,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    physics: {
+        default: 'P2',
+        arcade: {
+            debug: false
+        }
+    }
+};
 
-//
-// Engine script loader
-//
+let game = new Phaser.Game(config);
 
-function include(url) {
-  
-    var sc = document.createElement('script');
-    sc.src = url;
-    document.head.appendChild(sc);
-  }
-  
-  include('js/background.js');
-  include('js/phaser.js');
-  include('js/peasant.js');
-  
+
+function preload() {
+    this.load.image('ground','assets/Background.jpg');
+    this.load.spritesheet('castle','assets/Kasteel.png', {frameWidth: 64, frameHeight: 64});
+}
+
+function create() {
+    this.add.image(350,350,'ground');
+    let center = this.physics.add.sprite(350,350,'castle');
+    center.scale.setTo(2,2);
+}
+
+function update() {
+
+}
