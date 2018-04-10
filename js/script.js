9c3ad2cadd825c67dda220d8a53dc853ee72c2df
Unknown <mathias.caelenberghe@student.howest.be>
2018-04-10 17:46:26 +0200

    kerplunk

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const config = {
 };
 
 let game = new Phaser.Game(config);
-
+let knight;
 
 function preload() {
     this.load.image('ground','assets/Background.jpg');
@@ -26,17 +26,32 @@ function preload() {
     this.load.spritesheet('knight','assets/Ridder.png', {frameWidth:64, frameHeight: 64});
 }
 
-function create() {;
+function create() {
     this.add.image(350,350,'ground');
     let center = this.add.sprite(350,350,'castle');
-    let knight = this.add.sprite(400,350,'knight');
-    knight.scaleX = 1.5;
-    knight.scaleY = 1.5
-
+    knight = this.add.sprite(400,350,'knight');
+    knight.scaleX = 1.6;
+    knight.scaleY = 1.6;
+    knight.setCollideWorldBounds = true;
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('knight', {start:0, end:1}),frameRate: 5,
+        repeat: -1
+    });
     let peasent = this.add.sprite(200,200,'peasant');
     let squirrel = this.add.sprite(100,200, 'squirrel');
 }
 
 function update() {
+    let cursors = this.input.keyboard.createCursorKeys();
+    if(knight.x<650) {
+        knight.x += 1;
+
+        knight.anims.play('right', true);
+    }
+    else
+    {
+        
+    }
 
 }
