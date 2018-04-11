4edbd1cb21f6ba78bc7d1e813d4d5ef8076c3fe4
A.David <crocobatosaur@gmail.com>
2018-04-11 09:27:56 +0200

    ridder moves with arrows

--- js/script.js
+++ js/script.js
@@ -8,9 +8,12 @@ const config = {
         update: update
     },
     physics: {
-        default: 'p2',
-        p2: {
-            debug: false
+        default: 'matter',
+        matter: {
+            gravity: {
+                x:0,
+                y:0
+            }
         }
     }
 };
@@ -22,14 +25,54 @@ function preload() {
     this.load.image('ground','assets/Background.jpg');
     this.load.spritesheet('castle','assets/Kasteel.png', {frameWidth: 64, frameHeight: 64});
     this.load.spritesheet('peasant','assets/Peasant.png', {frameWidth: 64, frameHeight: 64});
+    this.load.spritesheet('ridder','assets/Ridder.png', {frameWidth: 64, frameHeight: 64});
 }
+
+var ridder;
+var cursors;
 
 function create() {
+    var bounds = new Phaser.Geom.Rectangle(100, 100, 400, 400);
+    // creates bounds
     this.add.image(350,350,'ground');
     let center = this.add.sprite(350,350,'castle');
-    let peasent = this.add.sprite(200,200,'peasant');
+    ridder = this.matter.add.sprite(200,200,'ridder');
+    
+    /*
+    for (var i = 0; i < 20; i++){
+        var peasant =  this.add.sprite(100+i*Math.floor((Math.random() * 10) + 1),100+i*Math.floor((Math.random() * 10) + 1),'peasant')
+    }
+    */
+    //  If you don't set the body as active it won't collide with the world bounds
+    this.matter.add.image(300, 300, 'peasant').setActive().setVelocity(300, 200).setBounce(1);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {
 
+function update() {
+    if (cursors.left.isDown)
+    {
+        ridder.setVelocityX(-10);
+    }
+    else if (cursors.right.isDown)
+    {
+        ridder.setVelocityX(10);
+    }
+    else
+    {
+        ridder.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown)
+    {
+        ridder.setVelocityY(-10);
+    }
+    else if (cursors.down.isDown)
+    {
+        ridder.setVelocityY(10);
+    }
+    else
+    {
+        ridder.setVelocityY(0);
+    }
 }
